8dcc17e80325c3d6cca7c0fb4ae62158739bd64d
Toms Upis <toms.upizs@gmail.com>
2022-06-07 23:13:25 +0300

    Added new class to keep methods related to screen/UI

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -22,6 +23,8 @@ namespace Snake
     {
         private const int MapWidth = 60;
         private const int MapHeight = 40;
+
+        private static ScreenOperations _screenOperations;
 
         private static Stopwatch timePlayed = new Stopwatch();
 
@@ -38,75 +41,22 @@ namespace Snake
 
         private static void Main(string[] args)
         {
-            ConsoleSettings();
+            _screenOperations = new ScreenOperations(mapWidth: 60, mapHeight: 40);
             while (true)
             {
-                DrawBorder();
-                DefaultConsoleColors();
-
                 Welcome();
             }
         }
 
-        private static void DefaultConsoleColors()
-        {
-            BackgroundColor = ConsoleColor.Black;
-            ForegroundColor = ConsoleColor.White;
-        }
-
-        private static void ConsoleSettings()
-        {
-            SetWindowSize(MapWidth, MapHeight);
-            SetBufferSize(MapWidth, MapHeight);
-            CursorVisible = false;
-        }
-
-        private static void DrawBorder()
-        {
-            // This method uses 17sec to draw 1000 cycles from which 2s is Console.Clear()
-            // In total it's x4.4 times faster than second method.
-            Console.Clear();
-
-            ForegroundColor = ConsoleColor.White;
-            var whiteSpace = new StringBuilder();
-            var blackSpace = new StringBuilder();
-            whiteSpace.Append('#', WindowWidth - 1);
-            blackSpace.Append(' ', WindowWidth - 3);
-
-            SetCursorPosition(0, 0);
-            var sb = new StringBuilder();
-            sb.AppendLine($"{whiteSpace}");
-            for (int i = 1; i < MapHeight - 2; i++)
-            {
-                SetCursorPosition(0, i);
-                sb.Append("#");
-                sb.Append($"{blackSpace}");
-                sb.AppendLine("#");
-            }
-            sb.AppendLine($"{whiteSpace}");
-            Console.Write(sb);
-
-            //Cooling the 'cw gun'. Draws border a bit smoother.
-            Thread.Sleep(50);
-
-            //  Old simple and relatively slow method  to write each block [1000 cycles = 67sec]
-            //  for (int i = 0; i < MapHeight; i++)
-            //      {
-            //      new Pixel(0, i, ConsoleColor.White).Draw();
-            //      new Pixel(MapWidth - 1, i, ConsoleColor.White).Draw();
-            //      }
-            //  for (int i = 0; i < MapWidth; i++)
-            //      {
-            //      new Pixel(i, 1, ConsoleColor.White).Draw();
-            //      new Pixel(i, MapHeight - 1, ConsoleColor.White).Draw();
-            //      }
-        }
-
         private static void Welcome()
         {
-            WriteText("THE SNAKE!");
-            WriteText("Use Arrows to direct the Snake", 2);
-            WriteText(" Press SPACE to start and pause the Game", 4);
+
+            var welcomeTexts = new List<string>();
+            welcomeTexts.Add("THE SNAKE!");
+            welcomeTexts.Add("Use Arrows to direct the Snake");
+            welcomeTexts.Add(" Press SPACE to start and pause the Game");
+
+            _screenOperations.WriteText(welcomeTexts);
             //Gives the player time to read the message
             Thread.Sleep(500);
 
@@ -121,17 +71,16 @@ namespace Snake
             //Start the game when user input is Space
 
             //When the game starts clean the above text
-            ClearText();
-            ClearText(2);
-            ClearText(4);
+            _screenOperations.ClearText(welcomeTexts.Count());
+            
             //3 2 1 + Beeps
             for (int l = 3; l > 0; l--)
             {
                 Task.Run(() => Beep(1500, 100));
-                WriteText(l.ToString());
+                _screenOperations.WriteText(l.ToString());
                 Thread.Sleep(500);
 
-                ClearText();
+                _screenOperations.ClearText(1);
                 Thread.Sleep(400);
             }
 
@@ -207,7 +156,6 @@ namespace Snake
                 Direction oldMovement = currentMovement;
 
                 while (sw.ElapsedMilliseconds <= GameSpeedDelay - lagMs)
-
                 {
                     if (oldMovement == currentMovement)
                     {
@@ -237,18 +185,21 @@ namespace Snake
             // GameOver Procedure.
             snake.Clear();
             food.Erase();
-            DefaultConsoleColors();
-            SetCursorPosition(MapWidth / 2 - 5, MapHeight / 3);
-            Console.Write("Game Over!");
-            SetCursorPosition(MapWidth / 2 - 6, MapHeight / 3 + 2);
-            timePlayed.Stop();
+
+            //Wasnt sure if you want to keep that one broken space where the snake hit before resetting screen,
+            //if so then can uncomment default colors in delete Reset screen here and uncomment the one at the end of this method
+            //_screenOperations.DefaultConsoleColors();
+            _screenOperations.ResetScreen();
+
+            var gameOverTexts = new List<string>();
+            gameOverTexts.Add("Game Over!");
             TimeSpan timeSpan = TimeSpan.FromSeconds(Convert.ToInt32(timePlayed.Elapsed.TotalSeconds));
-            Console.Write("Time : " + timeSpan.Minutes + "m " + timeSpan.Seconds + "s");
-            SetCursorPosition(MapWidth / 2 - 5, MapHeight / 3 + 4);
-            if (Score < 10)
-                Write("Score : " + Score + "$");
-            else { Write("Score: " + Score + "$"); }
+            gameOverTexts.Add("Time : " + timeSpan.Minutes + "m " + timeSpan.Seconds + "s");
+            gameOverTexts.Add("Score : " + Score + "$");
+            _screenOperations.WriteText(gameOverTexts);
+
             GameOverTune();
+            //_screenOperations.ResetScreen();
         }
 
         private static void GameOverTune()
@@ -283,49 +234,6 @@ namespace Snake
             }
         }
 
-        /// <summary>
-        /// Writes a text in the middle of the console
-        /// </summary>
-        /// <param name="text"></param>
-        private static void WriteText(string text)
-        {
-            var length = text.Length;
-
-            SetCursorPosition(MapWidth / 2 - length / 2, MapHeight / 3);
-            Console.WriteLine(text);
-        }
-
-        /// <summary>
-        /// Writes a text in the middle of the console
-        /// </summary>
-        /// <param name="text"></param>
-        private static void WriteText(string text, int yOffset)
-        {
-            var length = text.Length;
-
-            SetCursorPosition(MapWidth / 2 - length / 2, MapHeight / 3 + yOffset);
-            Console.WriteLine(text);
-        }
-
-        /// <summary>
-        /// Replaces the text written with line of whitespace
-        /// </summary>
-        /// <param name="length"></param>
-        private static void ClearText()
-        {
-            SetCursorPosition(1, MapHeight / 3);
-            Console.WriteLine(new string(' ', MapWidth - 3));
-        }
-
-        /// <summary>
-        /// Replaces the text written with line of whitespace
-        /// For lines underneath
-        /// </summary>
-        /// <param name="length"></param>
-        private static void ClearText(int yOffset)
-        {
-            SetCursorPosition(1, MapHeight / 3 + yOffset);
-            Console.WriteLine(new string(' ', MapWidth - 3));
-        }
+        
     }
 }
